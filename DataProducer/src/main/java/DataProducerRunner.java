745345fdbfa337f8fe9ac79d9030ddf0b41a37ad
kevin0988459 <kevin0988459@gmail.com>
2024-11-08 19:35:55 -0500

    tracefile

--- DataProducer/src/main/java/DataProducerRunner.java
+++ DataProducer/src/main/java/DataProducerRunner.java
@@ -11,14 +11,15 @@ public class DataProducerRunner {
     public static void main(String[] args) throws Exception {
         // Kafka Config
         Properties p = new Properties();
-        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.88.233:9092");
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.86.55:9092");
         p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         p.put(ProducerConfig.ACKS_CONFIG, "all");
         p.put(ProducerConfig.RETRIES_CONFIG, 3);
 
         Producer<String, String> producer = new KafkaProducer<>(p);
-        String traceFilePath = "../tracefile";
+        String traceFilePath = "~/stream-processing/DataProducer/tracefile";
+        System.out.println("Starting to send data to topic: driver-locations");
         DataProducer dataProducer = new DataProducer(producer, traceFilePath);
         dataProducer.sendData();
     }
