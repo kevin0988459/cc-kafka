48a3b02e85eee34bdb8021fbcf1a192a8e3ed446
kevin0988459 <kevin0988459@gmail.com>
2024-11-10 04:56:29 -0500

    bonus

--- DataProducer/src/test/java/DataProducerTest.java
+++ DataProducer/src/test/java/DataProducerTest.java
@@ -35,7 +35,9 @@ public class DataProducerTest {
 
         List<ProducerRecord<String, String>> history = producer.history();
 
-        List<ProducerRecord<String, String>> expected = Arrays.asList();
+        List<ProducerRecord<String, String>> expected = Arrays.asList(
+                new ProducerRecord<>("events", 3, null, "{\"blockId\":5648,\"type\":\"ENTERING_BLOCK\"}"),
+                new ProducerRecord<>("driver-locations", 4, null, "{\"blockId\":5649,\"type\":\"DRIVER_LOCATION\"}"));
 
         Assert.assertEquals("Producer records not matched!", expected, history);
     }
